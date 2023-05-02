0690ed41cce35d1c34fcbf0cc7e823bdaadf6dbb
samsulalamasif <samsulalamasif@gmail.com>
2023-05-02 18:14:22 +0600

    single chef details added

--- index.js
+++ index.js
@@ -9,10 +9,19 @@ const chefs = require("./data/chefs.json")
 app.get("/chefs", (req, res) => {
     res.send(chefs)
 })
+app.get("/chefs/:id", (req, res) => {
+    const id = req.params.id
+    const singleChef = chefs.find(chef => chef._id === id)
+    res.send(singleChef)
+})
+
+
+
 
 app.get("/", (req, res) => {
     res.send("Chef's Choice Server Side Running")
 })
+
 
 app.listen(port, () => {
     console.log(`chefs choice server side: ${port}`);
