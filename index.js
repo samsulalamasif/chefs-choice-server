5ce25e0fd0ab2f1ba0f0363dafedfbfb17e356bd
samsulalamasif <samsulalamasif@gmail.com>
2023-05-02 14:32:14 +0600

    chefs choice server side added

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require("express")
+const app = express()
+const port = process.env.PORT || 5000
+const cors = require("cors")
+app.use(cors())
+
+app.get("/", (req, res) => {
+    res.send("Chef's Choice Server Side Running")
+})
+
+app.listen(port, () => {
+    console.log(`chefs choice server side: ${port}`);
+})
