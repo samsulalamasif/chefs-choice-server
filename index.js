68ed4fe9868f8f5774765e3d293cfd47341a8dbb
samsulalamasif <samsulalamasif@gmail.com>
2023-05-02 14:55:37 +0600

    json data test

--- index.js
+++ index.js
@@ -4,6 +4,12 @@ const port = process.env.PORT || 5000
 const cors = require("cors")
 app.use(cors())
 
+const chefs = require("./data/chefs.json")
+
+app.get("/chefs", (req, res) => {
+    res.send(chefs)
+})
+
 app.get("/", (req, res) => {
     res.send("Chef's Choice Server Side Running")
 })
